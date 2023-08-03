cf3939b0113a1504ea5762d98b87f68affb0bb27
Carlos Sala Samper <info@gamesmayer.com>
2023-08-03 15:37:41 +0200

    Autoinject controllers

--- src/core/presentation/decorators/controller.ts
+++ src/core/presentation/decorators/controller.ts
@@ -1,10 +1,27 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { injectable } from "inversify";
 import ControllerConfig from "../types/ControllerConfig";
+import { Controller, ControllerToken } from "../types";
+import container from "../../../container";
 
 export default function controller(config: ControllerConfig) {
-  return <T extends { new (...args: any[]): object }>(constructor: T) => {
+  return <
+    T extends {
+      new (...args: any[]): {
+        handler: Controller["handler"];
+        middlewares: Controller["middlewares"];
+        lateMiddlewares: Controller["lateMiddlewares"];
+      };
+    }
+  >(
+    constructor: T
+  ) => {
     injectable()(constructor);
+
+    container
+      .bind<Controller>(ControllerToken)
+      .to(constructor)
+      .inSingletonScope();
 
     return class extends constructor {
       method = config.method.toLowerCase();
