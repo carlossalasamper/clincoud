22554595180c014c316e98b242623feaaf3ec602
Carlos Sala Samper <info@gamesmayer.com>
2023-08-03 17:47:29 +0200

    0.1.22

--- src/core/index.ts
+++ src/core/index.ts
@@ -6,12 +6,9 @@ export * from "./application";
 export * from "./infrastructure";
 export * from "./presentation";
 
-const boot = <AppType extends BaseApp>(App: AppType) => {
-  container
-    .bind<BaseApp>(typeof App)
-    .toSelf()
-    .inSingletonScope();
-  container.get<BaseApp>(typeof App).initialize();
+const boot = <AppType extends typeof BaseApp>(App: AppType) => {
+  container.bind<BaseApp>(App).toSelf().inSingletonScope();
+  container.get<BaseApp>(App).initialize();
 };
 
 export { boot };
