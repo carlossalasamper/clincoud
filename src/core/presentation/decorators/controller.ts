0c009a014194d710ea6c9ad2041508a17115a9d0
Carlos Sala Samper <info@gamesmayer.com>
2023-08-03 14:40:16 +0200

    0.1.10

--- src/core/presentation/decorators/controller.ts
+++ src/core/presentation/decorators/controller.ts
@@ -4,14 +4,12 @@ import ControllerConfig from "../types/ControllerConfig";
 
 export default function controller(config: ControllerConfig) {
   return <T extends { new (...args: any[]): object }>(constructor: T) => {
-    const c = class extends constructor {
+    injectable()(constructor);
+
+    return class extends constructor {
       method = config.method.toLowerCase();
       api = config.api;
       path = config.path || "";
     };
-
-    injectable()(c);
-
-    return c;
   };
 }
--- src/core/presentation/types/TypedRequest.ts
+++ src/core/presentation/types/TypedRequest.ts
@@ -1,22 +1,31 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request } from "express";
 import { ParamsDictionary } from "express-serve-static-core";
-import { ZodType, ZodTypeDef } from "zod";
 
-export declare type TypedRequest<
-  TParams extends ZodType<any, ZodTypeDef, any>,
-  TQuery extends ZodType<any, ZodTypeDef, any>,
-  TBody extends ZodType<any, ZodTypeDef, any>
-> = Request<TParams, any, TBody, TQuery>;
+export declare type TypedRequest<TParams, TQuery, TBody> = Request<
+  TParams,
+  any,
+  TBody,
+  TQuery
+>;
 
-export declare type BodyTypedRequest<
-  TBody extends ZodType<any, ZodTypeDef, any>
-> = Request<ParamsDictionary, any, TBody, any>;
+export declare type BodyTypedRequest<TBody> = Request<
+  ParamsDictionary,
+  any,
+  TBody,
+  any
+>;
 
-export declare type ParamsTypedRequest<
-  TParams extends ZodType<any, ZodTypeDef, any>
-> = Request<TParams, any, any, any>;
+export declare type ParamsTypedRequest<TParams> = Request<
+  TParams,
+  any,
+  any,
+  any
+>;
 
-export declare type QueryTypedRequest<
-  TQuery extends ZodType<any, ZodTypeDef, any>
-> = Request<ParamsDictionary, any, any, TQuery>;
+export declare type QueryTypedRequest<TQuery> = Request<
+  ParamsDictionary,
+  any,
+  any,
+  TQuery
+>;
